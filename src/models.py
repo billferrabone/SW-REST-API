6df90452b36b91d29440992e441ce3df82cfa40e
billferrabone <billferrabone@gmail.com>
2023-02-17 20:04:28 +0000

    guardar

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__="user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -16,4 +17,21 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+class Planet(db.Model):
+    __tablename__="planet"
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(120), nullable=False)
+    gravity=db.Column(db.Float, nullable=False)
+    created_by_id=db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_by=db.relationship("user.id")
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gravity": self.gravity,
         }
